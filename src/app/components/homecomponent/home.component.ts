6a1b38bfe6c92af98534b52853086659114b4d8d
en-bluncasu <bogdan.luncasu@endava.com>
2017-10-06 23:28:17 +0300

    wood cutter ui added - completed

--- src/app/components/homecomponent/home.component.ts
+++ src/app/components/homecomponent/home.component.ts
@@ -26,8 +26,10 @@ export class HomeComponent implements OnInit {
 
     
     preload() {   
-        this.game.load.spritesheet('castle', 'assets/game/castle/3.png',436,280);    
-        this.game.load.image('background', 'assets/game/background.png');    
+        this.game.load.spritesheet('castle', 'assets/game/castle/3.png',436,280);
+        this.game.load.spritesheet('woodcutter', 'assets/game/wood/2.png',266,154);    
+        this.game.load.image('background', 'assets/game/background.png');
+        //this.game.load.image('woodcutter' , 'assets/game/wood/1.png');    
         //this.game.load.image('castle','assets/game/castle/3.gif');
         
     }
@@ -44,17 +46,29 @@ export class HomeComponent implements OnInit {
                 game.canvas.style.cursor = "default";
             },this);
         };
+
+        function getFrames(n){
+            let frames:number[] = new Array(200);
+            for(let i=0; i<n;i++){
+                frames[i]=i+1;
+            }
+            return frames;
+        }
         //let sprite = this.game.add.sprite(40, 100, 'farm');
         //sprite.animations.add('millwind',[25,26,27,28,29,30,31,7,8,9,10,11,12,13,14,15,16,17,18,19,20],10,true);
         //sprite.animations.play('millwind');
-        let frames:number[] = new Array(200);
-        for(let i=0; i<170;i++){
-            frames[i]=i+1;
-        }
+        let frames_castle:number[] = getFrames(171);
         let castle = this.game.add.sprite(700,0,'castle');
-        castle.animations.add('millwind',frames);
-        castle.animations.play('millwind',70,true)
+        castle.animations.add('dragonanimation',frames_castle);
+        castle.animations.play('dragonanimation',70,true)
         addToggleMouseEventsLisener(castle,this.game);
+
+        let frames_wood:number[] = getFrames(161);
+        //let woodcutter = this.game.add.image(60,240,'woodcutter');
+        let woodcutter = this.game.add.sprite(22,243,'woodcutter');
+        woodcutter.animations.add('cuttree',frames_wood);
+        woodcutter.animations.play('cuttree',200,true)
+        addToggleMouseEventsLisener(woodcutter,this.game);
         
     }
 
